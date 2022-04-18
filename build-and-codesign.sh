8ffa634811251426c78a163c5c0ec5f9d89ce8e6
Peter Wong <peter.wm.wong@gmail.com>
2022-04-17 20:34:07 -0500

    Fix code signing (for debugging) causing "lldb rpc server has exited unexpectedly" error.

--- build-and-codesign.sh
+++ build-and-codesign.sh
@@ -12,8 +12,9 @@ esac
 
 [ $BUILD_PROFILE == "release" ] && TARGET_DIR="release" || TARGET_DIR="debug"
 
+rm target/$TARGET_DIR/app_launched_by_xcode
+cp target/$TARGET_DIR/$2 target/$TARGET_DIR/app_launched_by_xcode
+
 rm -f /tmp/tmp.entitlements
 /usr/libexec/PlistBuddy -c 'Add :com.apple.security.get-task-allow bool true' /tmp/tmp.entitlements
-codesign -s - --entitlements /tmp/tmp.entitlements -f target/$TARGET_DIR/$2
-
-cp target/$TARGET_DIR/$2 target/$TARGET_DIR/app_launched_by_xcode
+codesign -s - --entitlements /tmp/tmp.entitlements -f target/$TARGET_DIR/app_launched_by_xcode
