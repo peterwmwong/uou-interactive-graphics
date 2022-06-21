a417f6c7c7f70d2458082fa29aaeca6ad93870c8
Peter Wong <peter.wm.wong@gmail.com>
2022-06-21 18:14:05 -0500

    scripts: asm generation updates output once (uses tmp file swap), to prevent too much code editor diff updates.

--- scripts/asm-gen.sh
+++ scripts/asm-gen.sh
@@ -4,6 +4,7 @@ set -e
 CRATE_NAME=$1
 OUTPUT_POSTFIX=$2
 CRATE_NAME_NORMALIZED=$(echo "$CRATE_NAME" | tr "-" "_")
+TMP_OUTPUT_PATH="/tmp/$CRATE_NAME-$OUTPUT_POSTFIX-TMP.js"
 OUTPUT_PATH="/tmp/$CRATE_NAME-$OUTPUT_POSTFIX.js"
 
 ADDITIONAL_CRATES="|metal"
@@ -21,26 +22,28 @@ set +e
 FUNCTION_LIST=$(get_function_list)
 set -e
 
-echo "/*" > "$OUTPUT_PATH"
+echo "/*" > "$TMP_OUTPUT_PATH"
 padding=$(printf '%0.1s' " "{1..9})
 while IFS= read -r line; do
     func=$(echo "$line" | cut -d, -f1)
     size=$(echo "$line" | cut -d, -f2)
-    printf "%s%s %s\n" "${padding:${#size}}" "$size" "$func" >> "$OUTPUT_PATH"
+    printf "%s%s %s\n" "${padding:${#size}}" "$size" "$func" >> "$TMP_OUTPUT_PATH"
 done <<< "$FUNCTION_LIST"
-echo "*/" >> "$OUTPUT_PATH"
+echo "*/" >> "$TMP_OUTPUT_PATH"
 
 
-echo "export default {" >> "$OUTPUT_PATH"
+echo "export default {" >> "$TMP_OUTPUT_PATH"
 while IFS= read -r line; do
     func=$(echo "$line" | cut -d, -f1)
     size=$(echo "$line" | cut -d, -f2)
-    echo "/* $size */ \"$func\":\`" >> "$OUTPUT_PATH"
+    echo "/* $size */ \"$func\":\`" >> "$TMP_OUTPUT_PATH"
     cargo asm --release --lib -p "$CRATE_NAME" --full-name "$func" |
         tail -n +2 |                                       # Remove first line (function name, already printed above).
         egrep -v "^L(tmp|loh|BB)\d+" |                     # Remove lines that labels (diff noise reduction)
         sed 's/LBB\([0-9]\)*_\([0-9]\)*/LBB###/g' |        # Normalize all Branch Labels (diff noise reduction), ex. LBB123_1 -> LBB###
-        sed 's/Lloh\([0-9]\)*/Lloh###/g' >> "$OUTPUT_PATH" # Normalize all Lloh Addresses (diff noise reduction), ex. Lloh123 -> Lloh123
-    echo "\`," >> "$OUTPUT_PATH"
+        sed 's/Lloh\([0-9]\)*/Lloh###/g' >> "$TMP_OUTPUT_PATH" # Normalize all Lloh Addresses (diff noise reduction), ex. Lloh123 -> Lloh123
+    echo "\`," >> "$TMP_OUTPUT_PATH"
 done <<< "$FUNCTION_LIST"
-echo "}" >> "$OUTPUT_PATH"
+echo "}" >> "$TMP_OUTPUT_PATH"
+
+cp "$TMP_OUTPUT_PATH" "$OUTPUT_PATH"
